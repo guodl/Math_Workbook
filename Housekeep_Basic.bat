aa4958fdd6b21de06cb698c84aaead82df6a5702
guodl <guodl@users.noreply.github.com>
2020-12-09 17:52:41 +1300

    Add files via upload

---
+++ Housekeep_Basic.bat
@@ -0,0 +1,35 @@
+del /q *.aux
+del /q *.bbl
+del /q *.bcf
+del /q *.bib
+del /q *.blg
+del /q *.dvi
+del /q *.idx
+del /q *.ilg
+del /q *.ist
+del /q *.ind
+del /q *.listing
+
+del /q *.log
+
+del /q *.gnuplot
+
+del /q *.mylist
+del /q *.nav
+del /q *.out
+del /q *.out.ps
+del /q *.ptc
+del /q *.records
+del /q *.run.xml
+del /q *.snm
+del /q *.tcbtemp
+del /q *.tikz
+
+del /q *.tkzfonct.table
+
+del /q *.toc
+del /q *.xsim
+
+del /q *-exercise-body.tex
+del /q *-solution-body.tex
+
